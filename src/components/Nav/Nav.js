e727c27ceee424f886517811d34e176d5998d396
tenr <grodriguez1224@gmail.com>
2023-05-15 18:04:17 -0400

    added routes into nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,35 +14,40 @@ function Nav() {
           <ul className="menu menu-horizontal px-1">
             <li>
               <Link to="/events/all">
-                <a>Events</a>
+                <span>Events</span>
+              </Link>
+
+              <Link to="/about">
+                <span>About</span>
               </Link>
             </li>
             <li tabIndex={0}>
-              <a>
-                User
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2 bg-base-100 dropdown" id="drop-down">
-                <li>
-                  <a>Log-in</a>
-                </li>
-                <li>
-                  <a>Sign-up</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link to="/about">
-                <a>About</a>
+              <Link to="/profile">
+                <span className="flex">
+                  Profile
+                  <svg
+                    className="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                  </svg>
+                </span>
               </Link>
+              <ul className="p-2 bg-base-100 dropdown" id="drop-down">
+                <Link>
+                  <li>
+                    <span>Log-in</span>
+                  </li>
+                </Link>
+                <Link>
+                  <li>
+                    <span>Sign-up</span>
+                  </li>
+                </Link>
+              </ul>
             </li>
           </ul>
         </div>
