9eed89121bd4778eb221c1c7b8038c0407d9d849
Giancarlos Rodriguez <grodriguez1224@gmail.com>
2024-06-01 00:53:30 -0400

    styling on event details adjusted for mobile

--- src/pages/EventDetails/EventDetails.js
+++ src/pages/EventDetails/EventDetails.js
@@ -2,25 +2,18 @@ import React, { useState, useEffect, useContext } from "react";
 import AuthContext from "../../components/contexts/AuthContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { getDoc, doc, updateDoc, arrayUnion } from "firebase/firestore";
-import "./EventDetails.css";
 import { db } from "../../config/firebase";
-
-//We want to make the image be populated by firebase storage.. so import that
 
 function EventDetails(props) {
   const { event_id } = useParams();
   const [event, setEvent] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  /* ------------------------------- useContext for Auth ------------------------------- */
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  // console.log(user);
 
-  /* ---- Event Fetch ---- */
   const eventRef = doc(db, "events", event_id);
 
-  //make a call to the database with the params.id to get the specific doc with that id to set to state
   useEffect(() => {
     const getEventDetails = async () => {
       try {
@@ -37,7 +30,6 @@ function EventDetails(props) {
     getEventDetails();
   }, [loading]);
 
-  //Redirect to login/signup if no user signed in
   const handleReserve = async () => {
     setLoading(!loading);
     if (!user) {
@@ -60,7 +52,6 @@ function EventDetails(props) {
     }
   };
 
-  //handle payment
   const handlePaymentRedirect = () => {
     if (event && event.paymentLink) {
       window.open(event.paymentLink, "_blank");
@@ -71,32 +62,31 @@ function EventDetails(props) {
 
   return (
     <>
-      <div className="hero min-h-[640px] bg-base-200 flex flex-col">
+      <div className="hero min-h-[640px] bg-base-200 flex flex-col px-2  md:px-8 lg:px-10 xl:px-12">
         <div
-          className=" mt-12 gap-12 hero-content flex-col lg:flex-row-reverse"
+          className="mt-12 gap-12 hero-content flex-col lg:flex-row-reverse"
           id="wrapper"
         >
-          <img src={event?.image} className="max-w-sm  shadow-xl " />
+          <img src={event?.image} className="max-w-sm shadow-xl" alt="Event" />
           <div className="my-5">
-            <h1 className="text-5xl font-bold">{event?.activity}</h1>
+            <h1 className="text-4xl font-bold">{event?.activity}</h1>
             <p className="py-6">
               Come join us for another hard hitting Dodgeball game.
             </p>
 
             <ul>
               <p className="font-bold">Event Details:</p>
-              <li key={event}>★ {event?.date}</li>
+              <li>★ {event?.date}</li>
               <li>★ {event?.time}</li>
               <li>★ {event?.location}</li>
-              <br></br>
+              <br />
               <p className="font-bold">What to bring:</p>
               <li>★ water</li>
               <li>★ running shoes</li>
               <li>★ good vibes</li>
             </ul>
             <button
-              className="btn btn-lg btn-primary"
-              id="button"
+              className="btn btn-lg btn-primary mt-6 w-full md:w-1/2"
               onClick={() =>
                 event?.paid ? handlePaymentRedirect() : handleReserve()
               }
@@ -116,11 +106,10 @@ function EventDetails(props) {
           </div>
         </div>
 
-        {/*  ---------------------------------- Table --------------------------------- */}
-        <div>
+        <div className="mt-8">
           <h2 className="font-bold text-xl">Who's coming??</h2>
           <div className="overflow-x-auto">
-            <table className="table table-compact w-1/2">
+            <table className="table table-compact w-full lg:w-1/2 mx-auto">
               <thead>
                 <tr>
                   <th></th>
@@ -129,69 +118,25 @@ function EventDetails(props) {
                 </tr>
               </thead>
               <tbody>
-                {event?.participants?.map((participant, index) => {
-                  return (
-                    <tr>
-                      <th>{index + 1}</th>
-                      <td>{participant?.name}</td>
-                      <td>{participant?.nickname}</td>
-                    </tr>
-                  );
-                })}
+                {event?.participants?.map((participant, index) => (
+                  <tr key={index}>
+                    <th>{index + 1}</th>
+                    <td>{participant?.name}</td>
+                    <td>{participant?.nickname}</td>
+                  </tr>
+                ))}
               </tbody>
               <tfoot>
                 <tr>
                   <th></th>
-
-                  {/* <th>company</th>
-                  <th>location</th>
-                  <th>Last Login</th>
-                  <th>Favorite Color</th> */}
+                  <th>Name</th>
+                  <th>Nickname</th>
                 </tr>
               </tfoot>
             </table>
           </div>
         </div>
       </div>
-
-      {/*  ---------------------------------- Table --------------------------------- */}
-      {/* <div className="overflow-x-auto">
-        <table className="table table-compact w-1/2">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>company</th>
-              <th>location</th>
-              <th>Last Login</th>
-              <th>Favorite Color</th>
-            </tr>
-          </thead>
-          <tbody>
-            {event?.participants?.map((participant, i) => {
-              return (
-                <tr>
-                  <th>{i + 1}</th>
-                  <td>{participant.name}</td>
-                  <td>{participant.nickname}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>company</th>
-              <th>location</th>
-              <th>Last Login</th>
-              <th>Favorite Color</th>
-            </tr>
-          </tfoot>
-        </table>
-      </div> */}
     </>
   );
 }
